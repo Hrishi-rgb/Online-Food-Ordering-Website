4cbc48c9b27ad5f828abc61e4ba1f2dcfa5876ea
Hrishi-rgb <58764994+Hrishi-rgb@users.noreply.github.com>
2019-12-11 12:50:59 +0530

    Add files via upload

---
+++ Food-Ordering Website/index.php
@@ -0,0 +1,29 @@
+<HTML>
+<HEAD>
+<TITLE>Welcome to Online Food Ordering Website!!</TITLE>
+<style type="text/css">
+@import url(style.css);
+   a:link {color: #ffffff}
+   a:visited {color: #ffffff}
+   a:hover {color: #ffffff}
+   a:active {color: #ffffff}
+</style>
+</HEAD>
+<BODY background="download.jpg">
+<?php include("header.php"); ?>
+<FONT size="5" color="white">
+<SECTION align="center"><A HREF="index.php"></A></SECTION>
+<SECTION>
+<MAIN>
+
+<SECTION align="center"><IMG src="clickhere.gif" width="100" height="50"></IMG></SECTION>
+<TABLE >
+<TR><TD><FONT size="6"  color="white">
+<A HREF="order.php" style="text-decoration: none">Order Now!!!</A></FONT></TD></TR>
+
+</TABLE>
+</SECTION>
+</MAIN><BR>
+
+</BODY>
+</HTML>
